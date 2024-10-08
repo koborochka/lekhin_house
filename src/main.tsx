c332ae1dc3fa5533003812e2b8dc689ed6faa420
koborochka <098mishawifi@gmail.com>
2024-10-08 15:53:09 +0500

    карточка питомца+ стили

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 /*import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { IoPawSharp } from "react-icons/io5";
+import { IoGift } from "react-icons/io5";
 
 createRoot(document.getElementById('root')!).render(
   
