436e3d06e9ac22dea94519672192cb238fa33b05
koborochka <098mishawifi@gmail.com>
2024-10-31 21:40:15 +0500

    операции добавления изменения удаления питомцев

--- src/admin/AdminPage.tsx
+++ src/admin/AdminPage.tsx
@@ -4,13 +4,12 @@ import simpleRestProvider from 'ra-data-simple-rest';
 import pets from './pets'
 import Dashboard from './Dashboard';
 
-const dataProvider = simpleRestProvider('http://localhost:8080/api/'); // URL backend API
+const dataProvider = simpleRestProvider('http://localhost:8080/api');
 
 export default function AdminPage() {
   return (
-    <Admin dataProvider={dataProvider} dashboard={Dashboard}>
-        <Resource name="admin/pets" {...pets} />
-        <div>admin yep</div>
+    <Admin basename="/admin"  dataProvider={dataProvider} dashboard={Dashboard}>
+        <Resource {...pets} />
     </Admin>
   )
 }
--- src/admin/pets/index.tsx
+++ src/admin/pets/index.tsx
@@ -1,3 +1,4 @@
+import PetsIcon from '@mui/icons-material/Pets';
 import PetCreate from "./PetCreate";
 import PetEdit from "./PetEdit";
 import PetList from "./PetList";
@@ -6,4 +7,6 @@ export default {
     create: PetCreate,
     edit: PetEdit,
     list: PetList,
+    icon: PetsIcon,
+    name : "pet",    
 }
